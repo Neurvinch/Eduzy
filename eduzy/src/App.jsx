cc7cf0e1a3b7154a9e3fed792269fbdbcdffb641
Naveen pandian <npandian515@gmail.com>
2025-03-22 22:50:39 +0530

    add NFTrentAndMint component for NFT renting and minting functionality

--- eduzy/src/App.jsx
+++ eduzy/src/App.jsx
@@ -2,6 +2,7 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import './App.css'
 import ComeAndStake from './sections/ComeAndStake'
+import NFTrentAndMint from './sections/NFTrentAndMint'
 //import RegisterWithStake from './pages/RegisterWithStake'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
     {/* <RegisterWithStake/> */}
+    <NFTrentAndMint/>
     <ComeAndStake/>
       <ConnectButton/>
     </>
