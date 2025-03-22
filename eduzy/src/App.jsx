0253b37066b8c8c48f637970ffeafbafacdb412e
Your Name <your-email@example.com>
2025-03-22 23:21:02 +0530

    hhh

--- eduzy/src/App.jsx
+++ eduzy/src/App.jsx
@@ -1,17 +1,11 @@
 
-import { ConnectButton } from '@rainbow-me/rainbowkit'
+
 import './App.css'
-<<<<<<< HEAD
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-=======
-import ComeAndStake from './sections/ComeAndStake'
-//import RegisterWithStake from './pages/RegisterWithStake'
->>>>>>> 09ff9fdef9ac0124e97dabddd724a7351df8ad49
 
 function App() {
   return (
-<<<<<<< HEAD
     <div className="min-h-screen overflow-x-hidden">
       <Navbar />
       <main>
@@ -21,14 +15,6 @@ function App() {
       </main>
     </div>
   );
-=======
-    <>
-    {/* <RegisterWithStake/> */}
-    <ComeAndStake/>
-      <ConnectButton/>
-    </>
-  )
->>>>>>> 09ff9fdef9ac0124e97dabddd724a7351df8ad49
 }
 
 export default App;
