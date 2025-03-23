7eba7c4a9697cc26b3c9a363602bd1663edcb479
Naveen pandian <npandian515@gmail.com>
2025-03-23 07:49:51 +0530

    Refactor App component to include new game routes and update Navbar links

--- eduzy/src/App.jsx
+++ eduzy/src/App.jsx
@@ -1,38 +1,4 @@
 
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import ComeAndStake from './sections/ComeAndStake';
-import NFTrentAndMint from './sections/NFTrentAndMint';
-import MintingToken from './sections/MintingToken';
-import TokenSwap from './sections/Token-swap';
-import Events from './sections/Events';
-import Voting from './sections/Voting';
-import LiveStreaming from './sections/LiveStreaming';
-
-function App() {
-  return (
-    <div className="min-h-screen overflow-x-hidden">
-       <Router>
-      <Navbar />
-      <Routes>
-      <Route path="/" element={<Hero />} />
-        <Route path="/ComeAndStake" element={<ComeAndStake />} />
-        <Route path="/NFTrentAndMint" element={<NFTrentAndMint />} />
-        <Route path="/MintingToken" element={<MintingToken />} />
-        <Route path="/TokenSwap" element={<TokenSwap />} />
-        <Route path="/Events" element={<Events />} />
-        <Route path="/Voting" element={<Voting />} />
-        <Route path="/LiveStreaming" element={<LiveStreaming/>} />
-      </Routes>
-    </Router>
-      
-    </div>
-  );
-}
-
-export default App;
 
 
 import './App.css'
@@ -49,6 +15,7 @@ import Games from './components/GameSelector';
 import CoinCatcher from './components/CoinCatcher';
 import  WhackAMole from './components/WhackAMole';
 import Game2048  from './components/Game2048';
+import LiveStreaming from './sections/LiveStreaming';
 
 
 function App() {
@@ -68,6 +35,7 @@ function App() {
         <Route path ="/CoinCatcher" element={<CoinCatcher/>}/>
         <Route path ="/WhackAMole" element={<WhackAMole/>}/>
         <Route path ="/Game2048" element={<Game2048/>}/>
+        <Route path="/LiveStreaming" element={<LiveStreaming/>} />
         
       </Routes>
     </Router>
